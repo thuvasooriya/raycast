065eacecc30db075d0f5542138b26202d015cb2d
Thuva Sooriya <74165167+thuvasooriya@users.noreply.github.com>
2023-06-07 23:14:15 +0530

    new-window script
    Create a clone of any app you have. This is preconfigured for Orion Web Apps, change the path name for your apps.

---
+++ scripts/new-window.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title New Window
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🪟
+# @raycast.argument1 { "type": "text", "placeholder": "Web App" }
+
+# Documentation:
+# @raycast.author Thuva Sooriya
+# @raycast.authorURL https://github.com/thuvasooriya
+
+open -n "/Users/tony/Applications/Orion/WebApps/$1.app"
+
